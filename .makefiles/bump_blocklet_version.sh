432fcf9be940a7c080cf6867f07ca14ff2e8ac19
linchen1987 <linchen.1987@foxmail.com>
2022-08-12 12:55:03 +0800

    wip

--- .makefiles/bump_blocklet_version.sh
+++ .makefiles/bump_blocklet_version.sh
@@ -3,7 +3,7 @@
 NEW_VERSION=$(cat version | awk '{$1=$1;print}')
 
 cd website/blocklet-developer && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump blocklet server launcher to version $NEW_VERSION"
+echo "bump blocklet developer site to version $NEW_VERSION"
 
 cd website/blocklet-product && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump blocklet launcher to version $NEW_VERSION"
+echo "bump blocklet production site to version $NEW_VERSION"
