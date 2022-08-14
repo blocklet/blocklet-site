51424080b0a5032e11e5097cb93de392af659b12
wangshijun <wangshijun2010@gmail.com>
2022-08-15 07:25:02 +0800

    fix: bump version script [skip ci]

--- .makefiles/bump_blocklet_version.sh
+++ .makefiles/bump_blocklet_version.sh
@@ -3,7 +3,13 @@
 NEW_VERSION=$(cat version | awk '{$1=$1;print}')
 
 cd website/pages && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump blocklet developer site to version $NEW_VERSION"
+echo "bump platform site to version $NEW_VERSION"
+
+cd website/docs && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
+echo "bump platform docs to version $NEW_VERSION"
+
+cd developer/pages && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
+echo "bump developer site to version $NEW_VERSION"
 
 cd developer/docs && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump blocklet production site to version $NEW_VERSION"
+echo "bump developer docs to version $NEW_VERSION"
